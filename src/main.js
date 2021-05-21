8e2d149282bbbe600dbeb9e8115f3ba3c26bc96f
Tim Home <a0963573232@gmail.com>
2021-05-21 10:33:31 +0800

    Style: 編輯 style 架構

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router.js";
+import "./assets/scss/_reset.scss";
+import "./assets/scss/_global.scss";
 
 createApp(App)
   .use(router)
