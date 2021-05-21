cf96c9ca329f8008b9bb797db44aa1fb6a88a982
Tim Home <a0963573232@gmail.com>
2021-05-21 16:56:21 +0800

    Style: 新增 TaskList 架構

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -1,5 +1,46 @@
 <template>
-  <div></div>
+  <header class="task-header">
+    <input type="checkbox" name="" id="" />
+    <h2 class="title">Title...Title...Title...</h2>
+    <i class="far fa-star"></i>
+    <i class="fas fa-feather-alt"></i>
+  </header>
+  <div class="task-body">
+    <div class="inputGroup">
+      <label for="" class="inputGroup-title">
+        <i class="far fa-calendar-alt"></i>
+        Deadline
+      </label>
+      <div class="inputGroup-content">
+        <input type="date" name="" id="" />
+        <input type="time" name="" id="" />
+      </div>
+    </div>
+    <div class="inputGroup">
+      <label for="" class="inputGroup-title">
+        <i class="far fa-file-alt"></i>
+        File
+      </label>
+      <div class="inputGroup-content">
+        <input type="file" name="" id="" />
+      </div>
+    </div>
+    <div class="inputGroup">
+      <label for="" class="inputGroup-title">
+        <i class="far fa-comment"></i>
+        Comment
+      </label>
+      <div class="inputGroup-content">
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+      </div>
+    </div>
+  </div>
+  <footer class="task-footer">
+    <button class="task-footer-cancel">
+      <i class="fas fa-times"></i> Cancel
+    </button>
+    <button class="task-footer-save"><i class="fas fa-check"></i> Save</button>
+  </footer>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router.js";
 import "./assets/scss/_reset.scss";
 import "./assets/scss/_global.scss";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 createApp(App)
   .use(router)
