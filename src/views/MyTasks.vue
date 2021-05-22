55501ca2996c71fed898dbccb145cc3832d76675
Tim Home <a0963573232@gmail.com>
2021-05-22 17:26:12 +0800

    Feat: 由App.vue 統一載入 Tasks

--- src/views/MyTasks.vue
+++ src/views/MyTasks.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
-    <TaskItem v-for="task in db.tasks" :key="task.id" :task="task"></TaskItem>
+    <TaskItem v-for="task in tasks" :key="task.id" :task="task"></TaskItem>
   </div>
 </template>
 
 <script>
-import { defineComponent, reactive } from "vue";
+import { defineComponent } from "vue";
 import TaskItem from "@/components/TaskItem.vue";
 
 export default defineComponent({
@@ -13,39 +13,9 @@ export default defineComponent({
   components: {
     TaskItem,
   },
-  setup() {
-    const db = reactive({
-      tasks: [
-        {
-          id: 1,
-          title: "Task...Task...Task...",
-          date: "2021-05-22",
-          time: "15:22",
-          comment: "commtent...commtent...commtent...",
-          pinning: false,
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Task2...Task2...Task2...",
-          date: "2021-05-24",
-          time: "15:22",
-          comment: "commtent2...commtent2...commtent2...",
-          pinning: true,
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Task3...Task3...Task3...",
-          date: "2021-06-24",
-          time: "22:22",
-          comment: "commtent3...commtent3...commtent3...",
-          pinning: false,
-          completed: true,
-        },
-      ],
-    });
-    return { db };
+  props: {
+    tasks: { type: Object },
   },
+  setup() {},
 });
 </script>
