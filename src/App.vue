37692bf71e32b6eccf7c8c7916f23e35cdb19684
Tim Home <a0963573232@gmail.com>
2021-05-22 20:43:31 +0800

    Feat: 將 Tasks 讀取與儲存交由 LSUtils 處理

--- src/App.vue
+++ src/App.vue
@@ -8,47 +8,18 @@
       <router-link to="/Completed" class="title">Completed</router-link>
     </div>
   </header>
-  <router-view :tasks="db.tasks" />
+  <router-view :tasks="tasks" />
 </template>
 
 <script>
 import { defineComponent, reactive } from "vue";
+import { getTasks } from "./api/localStorageUtils";
 
 export default defineComponent({
   name: "App",
   setup() {
-    const db = reactive({
-      tasks: [
-        {
-          id: 1,
-          title: "Task...Task...Task...",
-          date: "2021-05-22",
-          time: "15:22",
-          comment: "commtent...commtent...commtent...",
-          pinning: false,
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Task2...Task2...Task2...",
-          date: "2021-05-24",
-          time: "15:22",
-          comment: "commtent2...commtent2...commtent2...",
-          pinning: true,
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Task3...Task3...Task3...",
-          date: "2021-06-24",
-          time: "22:22",
-          comment: "commtent3...commtent3...commtent3...",
-          pinning: false,
-          completed: true,
-        },
-      ],
-    });
-    return { db };
+    const tasks = reactive(getTasks());
+    return { tasks };
   },
 });
 </script>
---
+++ src/api/localStorageUtils.js
@@ -0,0 +1,38 @@
+const defaultTasks = [
+  {
+    id: 1,
+    title: "Default Tasks AAA",
+    date: "2021-05-22",
+    time: "03:22",
+    comment: "Default Tasks AAA\ncommtent1... commtent1... commtent1...",
+    pinning: false,
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "Default Tasks BBB",
+    date: "2021-05-24",
+    time: "15:22",
+    comment: "Default Tasks BBB\ncommtent2... commtent2... commtent2...",
+    pinning: true,
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Default Tasks CCC",
+    date: "2021-06-24",
+    time: "22:22",
+    comment: "Default Tasks CCC\ncommtent3... commtent3... commtent3...",
+    pinning: false,
+    completed: true,
+  },
+];
+
+const getTasks = () => {
+  return JSON.parse(localStorage.getItem("Tasks")) || defaultTasks;
+};
+const setTasks = (tasks) => {
+  localStorage.setItem("Tasks", JSON.stringify(tasks));
+};
+
+export { getTasks, setTasks };
